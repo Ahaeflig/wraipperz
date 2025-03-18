b501327cb0107ccd74077dfc2d50c50d34340f66
Adan Haefliger <adan.haefliger@gmail.com>
2025-03-18 15:30:21 +0900

    missing imports

--- src/wraipperz/__init__.py
+++ src/wraipperz/__init__.py
@@ -1,6 +1,8 @@
 from .api.llm import call_ai, call_ai_async
 from .api.messages import Message, MessageBuilder
 from .parsing import pydantic_to_yaml_example, find_yaml
+from .api.tts import create_tts_manager
+from .api.asr import create_asr_manager
 
 __all__ = [
     "call_ai",
@@ -9,4 +11,6 @@ __all__ = [
     "MessageBuilder",
     "pydantic_to_yaml_example",
     "find_yaml",
+    "create_tts_manager",
+    "create_asr_manager",
 ]
